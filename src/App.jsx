d188b4dba8e75708a0ae896f97cb1dbe6c8082a5
narayana <mandavalakshminarayana446@gmail.com>
2025-10-12 17:36:03 +0530

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ return(
+  <div>
+    <h2>Welcome to react app </h2>
+    <p>This is jenkins practice </p>
+  </div>
+ )
+}
+
+export default App
